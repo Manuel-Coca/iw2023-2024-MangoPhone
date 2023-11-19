706194655144d8af4370026d4301ed2f7f1caca7
Juan Garcia Candon <juangarcandon@gmail.com>
2023-11-19 17:50:05 +0100

    testing JunitTests

---
+++ src/main/java/es/uca/iw/aplication/tests/RegisterTest.java
@@ -0,0 +1,13 @@
+package es.uca.iw.aplication.tests;
+
+import es.uca.iw.aplication.tables.Cliente;
+import org.junit.jupiter.api.Test;
+
+public class RegisterTest {
+    private Cliente cliente = new Cliente();
+
+    @Test
+    public void testRegisterCliente(){
+
+    }
+}

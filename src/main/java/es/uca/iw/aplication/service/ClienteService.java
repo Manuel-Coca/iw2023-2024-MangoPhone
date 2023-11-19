2c4225b6796a37238b75f2524ca63cb9dcc06c22
Rafael Leal Pardo <falilealp@gmail.com>
2023-11-19 18:05:59 +0100

    Encriptación contraseña

--- src/main/java/es/uca/iw/aplication/service/ClienteService.java
+++ src/main/java/es/uca/iw/aplication/service/ClienteService.java
@@ -1,6 +1,7 @@
 package es.uca.iw.aplication.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import es.uca.iw.aplication.repository.ClienteRepository;
 import es.uca.iw.aplication.tables.Cliente;
@@ -9,12 +10,16 @@ import es.uca.iw.aplication.tables.Cliente;
 public class ClienteService {
     private final ClienteRepository clienteRepository;
 
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
     public ClienteService(ClienteRepository clienteRepository){
         this.clienteRepository = clienteRepository;
     }
 
-    public void createCliente(Cliente cliente) {         
+    public void createCliente(Cliente cliente) {       
+        cliente.setContrasena(passwordEncoder.encode(cliente.getContrasena()));   
+        
         clienteRepository.save(cliente);     
     }
 }

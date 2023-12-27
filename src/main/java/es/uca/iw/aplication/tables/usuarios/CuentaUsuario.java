7dcf9ead413d69aa793fd5f1a2121cccff5af894
Manuel-Coca <manuel.cocaalba@alum.uca.es>
2023-12-27 11:46:36 +0100

    Create CuentaUsuario.java

---
+++ src/main/java/es/uca/iw/aplication/tables/usuarios/CuentaUsuario.java
@@ -0,0 +1,16 @@
+package es.uca.iw.aplication.tables.usuarios;
+
+public class CuentaUsuario {
+    /*
+     * Usuario dueñoCuenta
+     * 
+     * List<Tarifa> tarifasContratadas
+     * 
+     * BigDecimal pagoMensual
+     * 
+     * boolean roaming
+     * boolean compartirDatos
+     * 
+     * List<String> numerosBloqueados
+     */
+}

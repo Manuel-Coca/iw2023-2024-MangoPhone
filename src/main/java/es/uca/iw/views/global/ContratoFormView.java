f6147a28b6cbf592d9aca3af14518c7c81931bbf
Manuel-Coca <manuel.cocaalba@alum.uca.es>
2023-12-27 01:06:50 +0100

    fin contrato view

--- src/main/java/es/uca/iw/views/global/ContratoFormView.java
+++ src/main/java/es/uca/iw/views/global/ContratoFormView.java
@@ -6,12 +6,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
@@ -32,7 +36,7 @@ public class ContratoFormView extends Div {
     private TarifaService tarifaService;
     
     private BigDecimal precioTotal = new BigDecimal(0);
-    private H3 precioTitle = new H3("Precio total: " + String.valueOf(precioTotal) + " €");
+    private H3 precioTitle = new H3("Precio total: " + String.valueOf(precioTotal) + " €/mes");
     
     // Manejadores de selección para cada servicio
     private SeleccionadorDeTarifas seleccionadorMovil = new SeleccionadorDeTarifas();
@@ -58,44 +62,80 @@ public class ContratoFormView extends Div {
         List<Div> cardsFibra = new ArrayList<>();
         List<Div> cardsFijo = new ArrayList<>();
 
-        for (Tarifa tarifa : tarifasMovil) {
+        for(Tarifa tarifa : tarifasMovil) {
             Div card = crearTarjeta(tarifa, seleccionadorMovil, cardsMovil);
             cardsMovil.add(card);
         }
 
-        for (Tarifa tarifa : tarifasFibra) {
+        for(Tarifa tarifa : tarifasFibra) {
             Div card = crearTarjeta(tarifa, seleccionadorFibra, cardsFibra);
             cardsFibra.add(card);
         }
 
-        for (Tarifa tarifa : tarifasFijo) {
+        for(Tarifa tarifa : tarifasFijo) {
             Div card = crearTarjeta(tarifa, seleccionadorFijo, cardsFijo);
             cardsFijo.add(card);
         }
         
-        // Agregar a vista
-        globalVerticalLayout.add(precioTitle);
+        // CREACION DE LAYOUT
+        Button confirmButton = new Button("Continuar");
+        confirmButton.addClassName("boton-contratar");
+        confirmButton.addClickListener(event -> {
+            verificarSeleccion();
+        });
+        HorizontalLayout headerBlock = new HorizontalLayout();
+        headerBlock.setAlignItems(Alignment.CENTER);
+        headerBlock.add(precioTitle, confirmButton);
+        
+        globalVerticalLayout.add(headerBlock);
 
+        H1 movilTitle = new H1("Selecciona tu tarifa de móvil");
         HorizontalLayout cardsLayoutMovil = new HorizontalLayout();
-        for (Div card : cardsMovil) {
+        for(Div card : cardsMovil) {
             cardsLayoutMovil.add(card);
         }
-        globalVerticalLayout.add(new H1("Selecciona tu tarifa de móvil"), cardsLayoutMovil);
+        globalVerticalLayout.add(movilTitle, cardsLayoutMovil);
 
+        H1 fibraTitle = new H1("Selecciona tu tarifa de fibra");
+        fibraTitle.addClassName("margen-top-mid");
         HorizontalLayout cardsLayoutFibra = new HorizontalLayout();
-        for (Div card : cardsFibra) {
+        for(Div card : cardsFibra) {
             cardsLayoutFibra.add(card);
         }
-        globalVerticalLayout.add(new H1("Selecciona tu tarifa de fibra"), cardsLayoutFibra);
+        globalVerticalLayout.add(fibraTitle, cardsLayoutFibra);
 
+        H1 fijoTitle = new H1("Selecciona tu tarifa de fijo");
+        fijoTitle.addClassName("margen-top-mid");
         HorizontalLayout cardsLayoutFijo = new HorizontalLayout();
-        for (Div card : cardsFijo) {
+        for(Div card : cardsFijo) {
             cardsLayoutFijo.add(card);
         }
-        globalVerticalLayout.add(new H1("Selecciona tu tarifa de fijo"), cardsLayoutFijo);
+        globalVerticalLayout.add(fijoTitle, cardsLayoutFijo);
 
         globalDiv.add(globalVerticalLayout);
         return globalDiv;
+    }
+
+    private void verificarSeleccion(){
+        try {
+            if(seleccionadorFibra.tarifaSeleccionada == null && seleccionadorFijo.tarifaSeleccionada == null && seleccionadorMovil.tarifaSeleccionada == null) {
+                ConfirmDialog errorDialog = new ConfirmDialog();
+                errorDialog.setHeader("Error");
+                errorDialog.setText("Debe seleccionar al menos una tarifa");
+                errorDialog.setConfirmText("Cerrar");
+                errorDialog.addConfirmListener(event -> { errorDialog.close(); });
+                errorDialog.open();
+            }
+            else {
+                // LOGICA DE AÑADIR CONTRATOS
+            }
+        }
+        catch(Exception e) {
+            ConfirmDialog errorDialog = new ConfirmDialog("Error", "No se ha conseguido realizar la peticion", "Reintentar", event -> {
+                UI.getCurrent().navigate("/contratar");
+            });
+            errorDialog.open();
+        }
     }
 
     private Div crearTarjeta(Tarifa tarifa, SeleccionadorDeTarifas seleccionador, List<Div> cards) {
@@ -103,27 +143,26 @@ public class ContratoFormView extends Div {
         card.addClassName("card-contrato");
 
         card.addClickListener(event -> {
-            boolean yaSeleccionada = card.hasClassName("card-contrato-clicked");
             // Deseleccionar todas las tarjetas del mismo servicio si se selecciona otra
-            if (!yaSeleccionada) {
-                for (Div otraCard : cards) {
+            if(!card.hasClassName("card-contrato-clicked")) {
+                for(Div otraCard : cards) {
                     otraCard.removeClassName("card-contrato-clicked");
                 }
             }
 
-            boolean seleccionada = seleccionador.seleccionarTarifa(tarifa);
-            if (seleccionada) {
+            Tarifa previaSeleccion = seleccionador.tarifaSeleccionada;
+            if(seleccionador.seleccionarTarifa(tarifa)) {
                 card.addClassName("card-contrato-clicked");
-                actualizarPrecioTotal(tarifa.getPrecio(), true);
+                actualizarPrecioTotal(tarifa.getPrecio(), previaSeleccion, true);
             } 
             else {
                 card.removeClassName("card-contrato-clicked");
                 seleccionador.deseleccionarTarifa();
-                actualizarPrecioTotal(tarifa.getPrecio(), false);
+                actualizarPrecioTotal(tarifa.getPrecio(), null, false);
             }
         });
 
-        if (tarifa.getServicio().equals(Servicio.FIJO)) {
+        if(tarifa.getServicio().equals(Servicio.FIJO)) {
             Paragraph capacidad = new Paragraph(String.valueOf(tarifa.getCapacidad()) + " minutos");
             card.add(capacidad);
         }
@@ -137,11 +176,16 @@ public class ContratoFormView extends Div {
         return card;
     }
 
-    private void actualizarPrecioTotal(BigDecimal precio, boolean suma) {
-        if(suma) precioTotal = precioTotal.add(precio);
+    private void actualizarPrecioTotal(BigDecimal precio, Tarifa previaSeleccion, boolean suma) {    
+        if(suma) 
+            if(previaSeleccion != null) {
+                precioTotal = precioTotal.add(precio);
+                precioTotal = precioTotal.subtract(previaSeleccion.getPrecio());
+            }
+            else precioTotal = precioTotal.add(precio);
         else precioTotal = precioTotal.subtract(precio);
         
-        precioTitle.setText("Precio total: " + String.valueOf(precioTotal) + " €");
+        precioTitle.setText("Precio total: " + String.valueOf(precioTotal) + " €/mes");
     }
 
     // Clase para manejar la selección de tarifas por servicio
@@ -149,7 +193,7 @@ public class ContratoFormView extends Div {
         private Tarifa tarifaSeleccionada;
 
         boolean seleccionarTarifa(Tarifa tarifa) {
-            if (tarifaSeleccionada == null || !tarifaSeleccionada.equals(tarifa)) {
+            if(tarifaSeleccionada == null || !tarifaSeleccionada.equals(tarifa)) {
                 tarifaSeleccionada = tarifa;
                 return true; // La tarifa ha sido seleccionada
             }

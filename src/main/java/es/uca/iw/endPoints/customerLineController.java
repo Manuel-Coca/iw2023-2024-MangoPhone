754e3bdf46d335d1a0098d1074c6514d2cc6d760
Rafael Leal Pardo <falilealp@gmail.com>
2024-01-07 17:47:23 +0100

    Arreglo customerLineController

--- src/main/java/es/uca/iw/endPoints/customerLineController.java
+++ src/main/java/es/uca/iw/endPoints/customerLineController.java
@@ -1,27 +1,26 @@
 package es.uca.iw.endPoints;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
-
+import es.uca.iw.aplication.tables.usuarios.Usuario;
 import reactor.core.publisher.Mono;
 
-@AnonymousAllowed
+//@AnonymousAllowed
 @RestController
-@RequestMapping("/CustomerLine")
+//@RequestMapping("/CustomerLine")
 public class customerLineController {
     private final CustomerLineService customerLineService;
 
     public customerLineController(CustomerLineService customerLineService) { this.customerLineService = customerLineService; }
 
-    @GetMapping("/Phones")
+    
+    /*@GetMapping("/Phones")
     @AnonymousAllowed
+    public List<CustomerLine> getAllLines() { return customerLineService.getAllLines(); }*/
+
     public List<CustomerLine> getAllLines() { return customerLineService.getAllLines(); }
 
-    @GetMapping("/post")
+    /*@GetMapping("/post")
     @AnonymousAllowed
     public void lineReg() { 
         CustomerLine customerLine = new CustomerLine();
@@ -30,23 +29,42 @@ public class customerLineController {
         customerLine.setCarrier("MangoPhone");
         customerLine.setPhoneNumber("444223344");
         customerLineService.lineRegister(customerLine);
+    }*/
+
+    public void lineReg(Usuario usuario) { 
+        CustomerLine customerLine = new CustomerLine();
+        customerLine.setName(usuario.getNombre());
+        customerLine.setSurname(usuario.getApellidos());
+        customerLine.setCarrier("MangoPhone");
+        customerLine.setPhoneNumber(usuario.getTelefono());
+        customerLineService.lineRegister(customerLine);
     }
 
-    @GetMapping("/OnePhone")
+    /*@GetMapping("/OnePhone")
     @AnonymousAllowed
     public CustomerLine getOneLine() { 
         String id = "aa636a3e-a0ae-4485-8053-8012ee2e7975";
         return customerLineService.getOneLine(id);
+    }*/
+
+    public CustomerLine getOneLine(Usuario usuario) { 
+        String id = getOneLineByPhoneNumber(usuario).getId();
+        return customerLineService.getOneLine(id);
     }
 
-    @GetMapping("/deleteLine")
+    /*@GetMapping("/deleteLine")
     @AnonymousAllowed
     public void deleteLine() { 
         String id = "aa636a3e-a0ae-4485-8053-8012ee2e7975";
         customerLineService.deleteLine(id);
+    }*/
+
+    public void deleteLine(Usuario usuario) { 
+        String id = getOneLineByPhoneNumber(usuario).getId();
+        customerLineService.deleteLine(id);
     }
 
-    @GetMapping("/modifyLine")
+    /*@GetMapping("/modifyLine")
     @AnonymousAllowed
     public Mono<CustomerLine> modifyLine() { 
         String id = "aa636a3e-a0ae-4485-8053-8012ee2e7975";
@@ -56,30 +74,55 @@ public class customerLineController {
         customerLine.setCarrier("MangoPhone");
         customerLine.setPhoneNumber("111223344");
         return customerLineService.realizarSolicitudPatch(id, customerLine);
+    }*/
+
+    public Mono<CustomerLine> modifyLine(Usuario usuario) { 
+        String id = getOneLineByPhoneNumber(usuario).getId();
+        CustomerLine customerLine = new CustomerLine();
+        customerLine.setName(usuario.getNombre());
+        customerLine.setSurname(usuario.getApellidos());
+        customerLine.setCarrier("MangoPhone");
+        customerLine.setPhoneNumber(usuario.getTelefono());
+        return customerLineService.realizarSolicitudPatch(id, customerLine);
     }
 
-    @PatchMapping("/dataUsage")
+    /*@PatchMapping("/dataUsage")
     @AnonymousAllowed
     public List<DataUsageRecord> dataUsageLine() {
         String id = "ce2476ee-cf17-44b8-98ff-0aba52bfcd6f";
         String startDate = ""; 
         String endDate = "";
         return customerLineService.dataUsageCustomer(id, startDate, endDate);
+    }*/
+
+    public List<DataUsageRecord> dataUsageLine(Usuario usuario, String startDate, String endDate) { 
+        String id = getOneLineByPhoneNumber(usuario).getId();
+        return customerLineService.dataUsageCustomer(id, startDate, endDate);
     }
 
-    @GetMapping("/callRecord")
+    /*@GetMapping("/callRecord")
     @AnonymousAllowed
     public List<CallRecord> callRecordLine() {
         String id = "aa636a3e-a0ae-4485-8053-8012ee2e7975";
         String startDate = "2024-01-02"; 
         String endDate = "2024-01-03";
         return customerLineService.callRecordCustomer(id, startDate, endDate);
+    }*/
+
+    public List<CallRecord> callRecordLine(Usuario usuario, String startDate, String endDate) { 
+        String id = getOneLineByPhoneNumber(usuario).getId();
+        return customerLineService.callRecordCustomer(id, startDate, endDate);
     }
 
-    @GetMapping("/PhoneLine")
+    /*@GetMapping("/PhoneLine")
     @AnonymousAllowed
     public CustomerLine getOneLineByPhoneNumber() {
         String phoneNumber = "111223344"; 
+        return customerLineService.getOneLineByPhoneNumber(phoneNumber);
+    }*/
+
+    public CustomerLine getOneLineByPhoneNumber(Usuario usuario) {
+        String phoneNumber = usuario.getTelefono(); 
         return customerLineService.getOneLineByPhoneNumber(phoneNumber);
     }
 }
